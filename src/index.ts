fc41e9bf65657d107e4b32b3637ef25e43f16b08
Nio.Zhang <929354247@qq.com>
2022-03-19 22:29:25 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import './style/index.less'
 import GameControl from './modules/GameControl'
 
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+ 
+var flag = IsPC(); 
+console.log(flag)
+if(flag){
+  (document.getElementById('mobile') as HTMLElement).remove()
+}
 new GameControl()
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'docs'),
-    publicPath: 'Gluttonous-Snake/',
     filename: 'bundle.js',
     environment: {
       arrowFunction: false,
